4f375ac236f55de09de19176308cd6c7887454e6
Meylin Reyes <reyesmeylin20@gmail.com>
2021-10-24 10:45:12 -0600

    Nueva rama Meylin

--- systemUsersParcial2/harold.cs
+++ systemUsersParcial2/harold.cs
@@ -9,7 +9,24 @@ namespace systemUsersParcial2
 {
     class harold
     {
-       
+        Connection con = new Connection();
+        Crud crud = new Crud();
+        public int _idTv { get; set; }
+        public string _marca { get; set; }
+        public string _modelo { get; set; }
+        public string _color { get; set; }
+        public int _size { get; set; }
+        public string _year { get; set; }
+
+
+        //metodo para retornar los registros de la tabla 
+        public MySqlDataReader getAllHarold()// se ve admin
+        {
+            string query = "SELECT tvId,marca,modelo,color,size,year FROM televisor";
+
+            //llamado al metodo select de la clase Crud
+            return crud.select(query);
+        }
 
     }
 }
--- systemUsersParcial2/meylin.cs
+++ systemUsersParcial2/meylin.cs
@@ -9,6 +9,22 @@ namespace systemUsersParcial2
 {
     class meylin
     {
-        
+        Connection con = new Connection();
+        Crud crud = new Crud();
+        public int _figuraid { get; set; }
+        public string _nombre { get; set; }
+        public string _angulos { get; set; }
+        public string _lados { get; set; }
+
+
+
+        //metodo para retornar los registros de la tabla 
+        public MySqlDataReader getAllMeylin()// se ve admin
+        {
+            string query = "SELECT figuraid,nombre,angulos,lados FROM meylin";
+
+            //llamado al metodo select de la clase Crud
+            return crud.select(query);
+        }
     }
 }
